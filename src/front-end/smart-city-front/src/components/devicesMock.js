041d38b330dabfb51f510b72edc3738ae78f61f7
Mayara Almeida <mayaraalmeida@alu.ufc.br>
2025-06-27 08:50:12 -0300

    ajustes gerais

--- src/front-end/smart-city-front/src/components/devicesMock.js
+++
@@ -1,6 +0,0 @@
-const mockDevices = [
-  { id: 1, name: "Sensor de Temperatura", type: "Temperatura", status: "Ativo", config: 22 },
-  { id: 2, name: "Lâmpada", type: "Iluminação", status: "Inativo", config: 75 },
-];
-
-export default mockDevices;
